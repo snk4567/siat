3a8bac4f09c4f520ed0ed8b885fe8f28b7a7f08b
snk4567 <30365066+snk4567@users.noreply.github.com>
2019-03-21 14:03:36 +0900

    Update Model.java

--- step04_miniProject/step04_miniProject/src/step04/model/Model.java
+++ step04_miniProject/step04_miniProject/src/step04/model/Model.java
@@ -1,3 +1,4 @@
+//
 package step04.model;
 
 import java.util.HashMap;
@@ -7,17 +8,17 @@ import step04.model.domain.StudentDTO;
 public class Model {
 	public static HashMap<Integer, StudentDTO> getAll() {
 		HashMap<Integer, StudentDTO> hm = new HashMap<>();
-		hm.put(1, new StudentDTO("±è¹ÎÀç ", "010-2533-3905"));
-		hm.put(2, new StudentDTO("±è¼¼Èñ ", "010-2161-8927"));
-		hm.put(3, new StudentDTO("±èÈñ¹Î ", "010-3413-3768"));
-		hm.put(4, new StudentDTO("·ùÁ¤Çö ", "010-2766-7523"));
-		hm.put(5, new StudentDTO("¹Ú°æÇı ", "010-7754-1552"));
-		hm.put(6, new StudentDTO("¹Ú»çÇö ", "010-4300-0214"));
-		hm.put(7, new StudentDTO("¼­Á¤ÀÍ ", "010-4574-7871"));
-		hm.put(8, new StudentDTO("¿°À±¼± ", "010-7387-7427"));
-		hm.put(9, new StudentDTO("¿À¿ä¼Á ", "010-7180-1153"));
-		hm.put(10, new StudentDTO("À±¼Ò¿ø ", "010-7484-3764"));
-		hm.put(11, new StudentDTO("ÀÓÁøÅÃ ", "010-9544-2085"));
+		hm.put(1, new StudentDTO("ê¹€ë¯¼ì¬ ", "010-2533-3905"));
+		hm.put(2, new StudentDTO("ê¹€ì„¸í¬ ", "010-2161-8927"));
+		hm.put(3, new StudentDTO("ê¹€í¬ë¯¼ ", "010-3413-3768"));
+		hm.put(4, new StudentDTO("ë¥˜ì •í˜„ ", "010-2766-7523"));
+		hm.put(5, new StudentDTO("ë°•ê²½í˜œ ", "010-7754-1552"));
+		hm.put(6, new StudentDTO("ë°•ì‚¬í˜„ ", "010-4300-0214"));
+		hm.put(7, new StudentDTO("ì„œì •ìµ ", "010-4574-7871"));
+		hm.put(8, new StudentDTO("ì—¼ìœ¤ì„  ", "010-7387-7427"));
+		hm.put(9, new StudentDTO("ì˜¤ìš”ì…‰ ", "010-7180-1153"));
+		hm.put(10, new StudentDTO("ìœ¤ì†Œì› ", "010-7484-3764"));
+		hm.put(11, new StudentDTO("ì„ì§„íƒ ", "010-9544-2085"));
 		
 		return hm;
 
